2b2185719d41b2078c8ad5166e3ab2db9763ac16
Sr. Souza <jorgejrdj1@gmail.com>
2022-01-06 22:01:44 -0300

    Front-end da Página Home - Parte I

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import { GetStaticProps } from 'next';
 
+import Link from 'next/link';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Header from '../components/Header';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,9 +27,48 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <main className={commonStyles.container}>
+        <Header />
+
+        <div className={styles.posts}>
+          <Link href="/">
+            <a className={styles.post}>
+              <strong> Algum título</strong>
+              <p> Pensando em sincronização em vez de ciclos de vida</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 Mar 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Jorge de Souza
+                </li>
+              </ul>
+            </a>
+          </Link>
+          <Link href="/">
+            <a className={styles.post}>
+              <strong> Algum título</strong>
+              <p> Algum parágrafo</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 Mar 2021
+                  <FiUser />
+                  Jorge de Souza
+                </li>
+              </ul>
+            </a>
+          </Link>
+        </div>
+      </main>
+    </>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
