1375131af00664da34842113cbd644f2cd02d366
Sr. Souza <jorgejrdj1@gmail.com>
2022-01-11 22:13:30 -0300

    finalizado

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,23 +1,25 @@
-/* eslint-disable no-return-assign */
 /* eslint-disable no-param-reassign */
+/* eslint-disable no-return-assign */
+/* eslint-disable react/no-danger */
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { format } from 'date-fns';
 import { RichText } from 'prismic-dom';
 import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
-import Prismic from '@prismicio/client';
-import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
-import Head from 'next/head';
+import Prismic from '@prismicio/client';
+import Link from 'next/link';
 import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import Comments from '../../components/Comments';
 
 interface Post {
   first_publication_date: string | null;
+  last_publication_date: string | null;
   data: {
     title: string;
     banner: {
@@ -35,10 +37,33 @@ interface Post {
 
 interface PostProps {
   post: Post;
+  navigation: {
+    prevPost: {
+      uid: string;
+      data: {
+        title: string;
+      };
+    }[];
+    nextPost: {
+      uid: string;
+      data: {
+        title: string;
+      };
+    }[];
+  };
   preview: boolean;
 }
 
-export default function Post({ post, preview }: PostProps): JSX.Element {
+export default function Post({
+  post,
+  navigation,
+  preview,
+}: PostProps): JSX.Element {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <h1>Carregando...</h1>;
+  }
+
   const totalWords = post.data.content.reduce((total, contentItem) => {
     total += contentItem.heading.split(' ').length;
 
@@ -46,16 +71,9 @@ export default function Post({ post, preview }: PostProps): JSX.Element {
     words.map(word => (total += word));
     return total;
   }, 0);
-
   const readTime = Math.ceil(totalWords / 200);
 
-  const router = useRouter();
-
-  if (router.isFallback) {
-    return <h1>Carregando...</h1>;
-  }
-
-  const formattedDate = format(
+  const formatedDate = format(
     new Date(post.first_publication_date),
     'dd MMM yyyy',
     {
@@ -63,11 +81,22 @@ export default function Post({ post, preview }: PostProps): JSX.Element {
     }
   );
 
+  const isPostEdited =
+    post.first_publication_date !== post.last_publication_date;
+
+  let editionDate;
+  if (isPostEdited) {
+    editionDate = format(
+      new Date(post.last_publication_date),
+      "'* editado em' dd MMM yyyy', às' H':'m",
+      {
+        locale: ptBR,
+      }
+    );
+  }
+
   return (
     <>
-      <Head>
-        <title>{`${post.data.title} | spacetraveling`}</title>
-      </Head>
       <Header />
       <img src={post.data.banner.url} alt="imagem" className={styles.banner} />
       <main className={commonStyles.container}>
@@ -77,7 +106,7 @@ export default function Post({ post, preview }: PostProps): JSX.Element {
             <ul>
               <li>
                 <FiCalendar />
-                {formattedDate}
+                {formatedDate}
               </li>
               <li>
                 <FiUser />
@@ -88,14 +117,15 @@ export default function Post({ post, preview }: PostProps): JSX.Element {
                 {`${readTime} min`}
               </li>
             </ul>
+            <span>{isPostEdited && editionDate}</span>
           </div>
+
           {post.data.content.map(content => {
             return (
               <article key={content.heading}>
                 <h2>{content.heading}</h2>
                 <div
                   className={styles.postContent}
-                  // eslint-disable-next-line react/no-danger
                   dangerouslySetInnerHTML={{
                     __html: RichText.asHtml(content.body),
                   }}
@@ -105,6 +135,28 @@ export default function Post({ post, preview }: PostProps): JSX.Element {
           })}
         </div>
 
+        <section className={`${styles.navigation} ${commonStyles.container}`}>
+          {navigation?.prevPost.length > 0 && (
+            <div>
+              <h3>{navigation.prevPost[0].data.title}</h3>
+              <Link href={`/post/${navigation.prevPost[0].uid}`}>
+                <a>Post anterior</a>
+              </Link>
+            </div>
+          )}
+
+          {navigation?.nextPost.length > 0 && (
+            <div>
+              <h3>{navigation.nextPost[0].data.title}</h3>
+              <Link href={`/post/${navigation.nextPost[0].uid}`}>
+                <a>Próximo post</a>
+              </Link>
+            </div>
+          )}
+        </section>
+
+        <Comments />
+
         {preview && (
           <aside>
             <Link href="/api/exit-preview">
@@ -112,7 +164,6 @@ export default function Post({ post, preview }: PostProps): JSX.Element {
             </Link>
           </aside>
         )}
-
       </main>
     </>
   );
@@ -123,6 +174,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
   const posts = await prismic.query([
     Prismic.Predicates.at('document.type', 'posts'),
   ]);
+
   const paths = posts.results.map(post => {
     return {
       params: {
@@ -148,9 +200,28 @@ export const getStaticProps: GetStaticProps = async ({
     ref: previewData?.ref || null,
   });
 
+  const prevPost = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.first_publication_date]',
+    }
+  );
+
+  const nextPost = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.last_publication_date desc]',
+    }
+  );
+
   const post = {
     uid: response.uid,
     first_publication_date: response.first_publication_date,
+    last_publication_date: response.last_publication_date,
     data: {
       title: response.data.title,
       subtitle: response.data.subtitle,
@@ -166,9 +237,16 @@ export const getStaticProps: GetStaticProps = async ({
       }),
     },
   };
+
   return {
     props: {
       post,
+      navigation: {
+        prevPost: prevPost?.results,
+        nextPost: nextPost?.results,
+      },
+      preview,
     },
+    revalidate: 1800,
   };
 };
